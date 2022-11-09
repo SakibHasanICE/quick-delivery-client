7fbe1327240d347a38ca197474b3156a085fdb74
Sakibhasan <hasansakib047@gmai.,com>
2022-11-09 11:19:32 +0600

    details added

--- src/components/ServiceDetails/ServiceDetails.js
+++ src/components/ServiceDetails/ServiceDetails.js
@@ -6,9 +6,7 @@ const ServiceDetails = () => {
   return (
     <div>
       <div>
-        <button className="block w-48 rounded-md bg-red-500 mx-auto mt-5 h-10 text-white font-bold text-xl">
-          Download{" "}
-        </button>
+        <p>ServiceDetails</p>
         <p className="text-3xl text-green-800 font-bold mt-2">{details.name}</p>
         <img className="w-7/12 mx-auto  mt-7 mb-3" src={details.img} alt="" />
         <p className="text-lg text-emerald-800 mb-11 font-medium mt-2 w-10/12 mx-auto text-justify">
